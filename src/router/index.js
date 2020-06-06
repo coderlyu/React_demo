2e8db4cf2abacf54f3505bf2db42254803739793
coderlyu <974257574@qq.com>
2020-06-06 23:16:30 +0800

    修改router路由为按需加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,45 @@
 
-export { default as ProductsContainer } from '../containers/ProductsContainer'
-export { default as CartContainer } from '../containers/CartContainer'
-export { default as Layout } from '../components/Layout/index'
-export { default as Cart } from '../components/Cart/index'
-export { default as Home } from '../components/Home/inde'
-export { default as Product } from '../components/Product/index'
-export { default as TodoList } from '../components/TodoList/index'
+// export { default as ProductsContainer } from '../containers/ProductsContainer'
+// export { default as CartContainer } from '../containers/CartContainer'
+// export { default as Layout } from '../components/Layout/index'
+// export { default as Cart } from '../components/Cart/index'
+// export { default as Home } from '../components/Home/inde'
+// export { default as Product } from '../components/Product/index'
+// export { default as TodoList } from '../components/TodoList/index'
+//pages>index.js
+import Loadable from 'react-loadable'
+// import Loading from './my-loading-component';
+// 意思是在加载的时候 显示 都加载组件没有组件的时候 我们可以写为
+const Loading = () => null;  //加载时不现实loading
+
+const ProductsContainer = Loadable({
+  loader: () => import('../containers/ProductsContainer'), //按需加载 点击时只加载一个页面
+  loading: Loading,
+});
+const CartContainer = Loadable({
+  loader: () => import('../containers/CartContainer'),
+  loading: Loading,
+});
+const Layout = Loadable({
+  loader: () => import('../components/Layout/index'),
+  loading: Loading,
+});
+const Cart = Loadable({
+  loader: () => import('../components/Cart/index'),
+  loading: Loading,
+});
+const Home = Loadable({
+  loader: () => import('../components/Home/inde'),
+  loading: Loading,
+});
+const Product = Loadable({
+  loader: () => import('../components/Product/index'),
+  loading: Loading,
+});
+const TodoList = Loadable({
+  loader: () => import('../components/TodoList/index'),
+  loading: Loading,
+});
+export {
+  ProductsContainer, CartContainer, Layout, Cart, Home, Product, TodoList
+}//将页面导出
