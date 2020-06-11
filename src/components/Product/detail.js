373bc4ffdf90e201153b0f9cb09b0bb559e6de45
coderlyu <974257574@qq.com>
2020-06-11 22:13:55 +0800

    添加商品详情

--- src/components/Product/detail.js
+++ src/components/Product/detail.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import { Spin } from 'antd'
+import { Spin, Card, Button } from 'antd'
 import { withRouter } from 'react-router-dom'
 import data from '../../api/products.json'
+import '../../assets/css/p_detail.css'
 class productDetail extends Component {
   constructor(props) {
     super(props)
@@ -43,17 +44,43 @@ class productDetail extends Component {
       this.mountd = false
     }
   }
+  routeBack = () => {
+    this.props.history.go(-1)
+  }
   render() {
     let { product } = this.state
-    console.log(product)
     return (
       <Spin spinning={this.loading}>
-        <h3>{product.name}</h3>
-        <p>{product.intro}</p>
-        <p>{product.id}</p>
-        <p>{product.img}</p>
-        <p>{product.price}</p>
-        <p>{product.oldPrice}</p>
+        <Card>
+          <article className="detail-wrapper">
+            <div className="product-detail">
+              <img src={product.img} alt={product.name} title={product.name} />
+            </div>
+            <section className="detail-right">
+              <h3
+                style={{
+                  textAlign: 'center',
+                  fontSize: '25px',
+                  fontWeight: '700'
+                }}
+              >
+                {product.name}
+              </h3>
+              <section>
+                <p>{product.intro}</p>
+                <section style={{ marginLeft: '30px', fontSize: '20px' }}>
+                  <span className="price">{product.price}</span>
+                  <s style={{ marginLeft: '10px' }}>{product.oldPrice}</s>
+                </section>
+              </section>
+              <div style={{ textAlign: 'end', marginTop: '40px' }}>
+                <Button type="danger" onClick={this.routeBack}>
+                  ADD TO CART
+                </Button>
+              </div>
+            </section>
+          </article>
+        </Card>
       </Spin>
     )
   }
